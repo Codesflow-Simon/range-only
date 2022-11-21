6c0f7b821a189c31409ffe02f4a8bf8afdd28a10
Simon Little <53613991+Codesflow-Simon@users.noreply.github.com>
2022-11-21 12:09:22 +1100

    Added GTSAM

--- main.cpp
+++ main.cpp
@@ -1,17 +1,46 @@
 #include "emulator.hpp"
 #include "logging.hpp"
 
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <eigen3/Eigen/Dense>
+
+using namespace gtsam;
+using namespace std;
+
+typedef NonlinearFactorGraph Graph;
+
 Emulator getEmulator(); // Function prototype
 Emulator emulator;
 Anchor tag;
 
-int main() {
-    emulator = getEmulator();
-    tag = Anchor( Vector3(0.5,0.3,0.2), "0000"); // Set actual tag location
+Symbol
 
-    init_log();
+Eigen::Matrix<double,10,3> anchorMatrix <<
+  1, 0, 0,
+  0, 1, 0,
+  0, 0, 1,
+ -1, 0, 0,
+  0,-1, 0,
+  0, 0,-1,
+ -1, 1, 0,
+  1, 0,-1,
+  0,-1, 1,
+  2,-1, 1;
+
+int main() {
+  emulator = getEmulator();
+  tag = Anchor( Vector3(0.5,0.3,0.2), "0000"); // Set actual tag location
+
+  init_log();
+    write_log("tag: ");
     write_log(tag.to_string_());
-    close_log();
+  close_log();
+
+  Graph graph;
+}
+
+void add_anchors(Graph graph) {
+  graph.addPrior()
 }
 
 
@@ -34,27 +63,15 @@ Emulator getEmulator() {
 
 // Doxygen mainpage
 
-/*! \mainpage range-only slam
+/*! \mainpage template
  *
  * \section intro_sec Introduction
  *
- * Contains implimentation of a simple factor graph with anchors at a single time instance
+ * This is the introduction.
  *
  * \section install_sec Installation
- * 
- * To build and run
- * \code{.sh}
- * mkdir build
- * cd build
- * cmake ..
- * make
- * ./main
- * \endcode 
- * 
- * Generate documentation
- * \code{.sh}
- * doxygen docs_conf
- * \endcode 
  *
- * \subsection
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
  */
